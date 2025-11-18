e1d728c05fc5885695d3490cdebc8420fd3f64c5
Saloni-2005 <chaudharysaloni2510@gmail.com>
2025-11-18 13:24:59 +0530

    Initial setup

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-g0ug#3id3(x41b@3gu_w1kkii!s!xvp4##2gfi52z56s(@0=a$"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
